5ccb2eeaefb662806d5b1187623a0d613d98b62a
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-17 17:09:14 +0530

    Add files via upload

---
+++ Vowel.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Vowel {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter char:");
+	String a= sc.next(); 	
+	switch(a)
+	{
+	case "a": System.out.println("vowel"); break;
+	case "e": System.out.println("vowel"); break;
+	case "i": System.out.println("vowel"); break;
+	case "o": System.out.println("vowel"); break;
+	case "u": System.out.println("vowel"); break;
+	default : System.out.println("Not Vowel");
+	}
+}}
